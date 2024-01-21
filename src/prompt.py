b3622cf338a43b944cc4d201e3e2fef01c7248af
Brian-Ckwu <sonny40920710@gmail.com>
2024-01-21 14:41:08 +0800

    feat: implement QuestionGeneratorPrompt

--- src/prompt.py
+++ src/prompt.py
@@ -192,14 +192,95 @@ class DDXPredictorPrompt(Prompt):
         return self.SHOT_SEPARATOR.join(chunks) + self.LINE_SEPARATOR
 
 class QuestionGeneratorPrompt(Prompt):
+    POSITIVE_CUE = "Positive clinical findings: "
+    NEGATIVE_CUE = "Negative clinical findings: "
+    DDX_CUE = "Ranked differential diagnosis: "
+    QUESTION_CUE = "Question to narrow down the differential diagnosis: "
 
     def __init__(
         self,
-        test_input: dict[str, str],
         instruction: str = None,
         shots: list[dict[str, str]] = None
     ):
-        super().__init__(test_input, instruction, shots)
+        super().__init__(instruction, shots)
+
+    def get_minimal_template(self) -> str:
+        """Returns the prefix prompt text built by minimal template.
+
+        Format:
+            [Instruction (optional)]
+            <Shot_separator>
+            [Symptoms_1]
+            [DDX_1]
+            [Q_1]
+            <Shot_separator>
+            ...
+            <Shot_separator>
+            [Symptoms_k]
+            [DDX_k]
+            [Q_k]
+            <Shot_separator>
+            [Symptoms_test]
+            [DDX_test]
+        """
+        chunks = list()
+        # Instruction
+        if self.instruction:
+            chunks.append(self.instruction)
+        # Few-shot examples
+        if self.shots:
+            for shot in self.shots:
+                pos = self.ITEM_SEPARATOR.join(shot["positive"]) if shot["positive"] else "None"
+                neg = self.ITEM_SEPARATOR.join(shot["negative"]) if shot["negative"] else "None"
+                ddx = self.ITEM_SEPARATOR.join(shot["ddx"])
+                q = shot["q"]
+                chunk = self.LINE_SEPARATOR.join([pos, neg, ddx, q])
+                chunks.append(chunk)
+        return self.SHOT_SEPARATOR.join(chunks)        
+
+    def get_minimal_prompt(self, pos: list[str], neg: list[str], ddx: list[str]) -> str:
+        chunks = [
+            self.minimal_template,
+            self.LINE_SEPARATOR.join([
+                self.ITEM_SEPARATOR.join(pos),
+                self.ITEM_SEPARATOR.join(neg),
+                self.ITEM_SEPARATOR.join(ddx)
+            ])
+        ]
+        return self.SHOT_SEPARATOR.join(chunks) + self.LINE_SEPARATOR
+
+    def get_semantic_template(self) -> str:
+        """Returns the prefix prompt text built by semantic template (containing semantic cues)."""
+        chunks = list()
+        # Instruction
+        if self.instruction:
+            chunks.append(self.instruction)
+        # Few-shot examples
+        if self.shots:
+            for shot in self.shots:
+                pos = self.ITEM_SEPARATOR.join(shot["positive"]) if shot["positive"] else "None"
+                neg = self.ITEM_SEPARATOR.join(shot["negative"]) if shot["negative"] else "None"
+                ddx = self.ITEM_SEPARATOR.join(shot["ddx"])
+                q = shot["q"]
+                chunk = self.LINE_SEPARATOR.join([
+                    self.POSITIVE_CUE + pos,
+                    self.NEGATIVE_CUE + neg,
+                    self.DDX_CUE + ddx,
+                    self.QUESTION_CUE + q
+                ])
+                chunks.append(chunk)
+        return self.SHOT_SEPARATOR.join(chunks)
+
+    def get_semantic_prompt(self, pos: list[str], neg: list[str], ddx: list[str]) -> str:
+        chunks = [
+            self.semantic_template,
+            self.LINE_SEPARATOR.join([
+                self.POSITIVE_CUE + self.ITEM_SEPARATOR.join(pos),
+                self.NEGATIVE_CUE + self.ITEM_SEPARATOR.join(neg),
+                self.DDX_CUE + self.ITEM_SEPARATOR.join(ddx)
+            ])
+        ]
+        return self.SHOT_SEPARATOR.join(chunks) + self.LINE_SEPARATOR
 
 # Unit tests
 if __name__ == "__main__":
@@ -212,15 +293,21 @@ if __name__ == "__main__":
         "max_output_tokens": 100,
         "stop_sequences": [SymptomExtractorPrompt.LINE_SEPARATOR]
     })
-    ddx_prompt_text = json.loads(Path("../prompts/doctor/multistage/ddx_predictor.json").read_text())
-    ddx_prompt = DDXPredictorPrompt(**ddx_prompt_text)
+    qg_prompt_text = json.loads(Path("../prompts/doctor/multistage/question_generator.json").read_text())
+    qg_prompt = QuestionGeneratorPrompt(**qg_prompt_text)
 
-    pos = ["cough", "chest pain"]
-    neg = ["fever"]
-    ddx_minimal_prompt = ddx_prompt.get_minimal_prompt(pos, neg)
-    ddx_semantic_prompt = ddx_prompt.get_semantic_prompt(pos, neg)
+    pos = ["cough"]
+    neg = ["dyspnea", "fever", "sore throat", "heartburn or acid reflux"]
+    ddx = [
+        "Bronchospasm / acute asthma exacerbation",
+        "Allergic sinusitis",
+        "Acute COPD exacerbation / infection",
+        "GERD"
+    ]
+    qg_minimal_prompt = qg_prompt.get_minimal_prompt(pos, neg, ddx)
+    qg_semantic_prompt = qg_prompt.get_semantic_prompt(pos, neg, ddx)
 
-    minimal_res = llm.generate(prompt=ddx_minimal_prompt)
-    semantic_res = llm.generate(prompt=ddx_semantic_prompt)
+    minimal_res = llm.generate(prompt=qg_minimal_prompt)
+    semantic_res = llm.generate(prompt=qg_semantic_prompt)
     print(f"Minimal response: {minimal_res}")
     print(f"Semantic response: {semantic_res}")
